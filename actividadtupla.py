f6f5846c5075f74633dd570924d33da6eed47aff
HumbertoMG <123428978+HumbertoMG@users.noreply.github.com>
2023-02-08 19:56:05 -0600

    Update actividadtupla.py

--- actividadtupla.py
+++ actividadtupla.py
@@ -1,7 +1,12 @@
-tupla = tuple(1,"X",2.35,"hola",2)
-print ("Tupla de 5 elementos: " + tupla)
+# Defino la tupla
+tupla = (1,"X",2.35,"hola",2)
+# Impresión de tupla
+print ("Tupla de 5 elementos: ", tupla)
+# Convierto la tupla en lista utilizando función list
 lista = list(tupla)
-# lista = [tupla(0), tupla(1), tupla(2), tupla(3), tupla(4)]
-print(Lista de la tupla de 5 elementos: " + lista)
-diccionario = {1:lista(0), 2: lista(1), 3: lista(2), 4: lista(3), 5: lista(4)}
-print ("Diccionario de la lista de 5 elementos: " + diccionario)
+# Imprimo la lista
+print("Lista de la tupla de 5 elementos: ", lista)
+# Defino diccionario con los elementos de la lista
+diccionario = {1:lista[0], 2: lista[1], 3: lista[2], 4: lista[3], 5: lista[4]}
+# Imprimo el diccionario
+print ("Diccionario de la lista de 5 elementos: ", diccionario)
