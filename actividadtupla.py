9c4adddd65acb67cf701b2c09139b778efb01a8c
HumbertoMG <123428978+HumbertoMG@users.noreply.github.com>
2023-02-08 19:17:22 -0600

    Create actividadtupla.py
    Actividad: Tupla
    
    1.- Crea una tupla con una longitud de 5 usando diferentes tipos de datos.
    2.- Cambiar dicha tupla a una lista.
    3.- Crea un diccionario donde la clave sea del 1 al 5 y los elementos los datos de la lista.
    Utilizar la función "list" para la elaboración de la actividad

---
+++ actividadtupla.py
@@ -0,0 +1,7 @@
+tupla = tuple(1,"X",2.35,"hola",2)
+print ("Tupla de 5 elementos: " + tupla)
+lista = list(tupla)
+# lista = [tupla(0), tupla(1), tupla(2), tupla(3), tupla(4)]
+print(Lista de la tupla de 5 elementos: " + lista)
+diccionario = {1:lista(0), 2: lista(1), 3: lista(2), 4: lista(3), 5: lista(4)}
+print ("Diccionario de la lista de 5 elementos: " + diccionario)
